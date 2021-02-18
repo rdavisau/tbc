88669c78ed93df9ff1f3c86174790f6feebb5543
Ryan Davis <ryandavis.au@gmail.com>
2021-02-18 20:38:03 +1000

    ci: pack tbc.core

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,7 @@
 dotnet publish -c Release -o output/win -r win-x64 -p:PublishSingleFile=true -p:DebugType=None src/heads/tbc.host.console/tbc.host.console.csproj
 dotnet publish -c Release -o output/macos -r osx-x64 -p:PublishSingleFile=true -p:DebugType=None src/heads/tbc.host.console/tbc.host.console.csproj
 
+dotnet build -c Release src/components/tbc.core/tbc.core.csproj
 dotnet build -c Release src/components/tbc.target/tbc.target.csproj
+dotnet pack -c Release -o output/nuget /p:PackageVersion=0.0.$BUILD_BUILDNUMBER src/components/tbc.core/tbc.core.csproj 
 dotnet pack -c Release -o output/nuget /p:PackageVersion=0.0.$BUILD_BUILDNUMBER src/components/tbc.target/tbc.target.csproj 
