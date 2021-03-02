96c37eb4736f56b1811b055e1cd369ec9a104ed7
Ryan Davis <ryandavis.au@gmail.com>
2021-03-02 13:46:01 +1000

    ci: pack tbc.host

--- build/build.sh
+++ build/build.sh
@@ -3,5 +3,8 @@ dotnet publish -c Release -o output/macos -r osx-x64 -p:PublishSingleFile=true -
 
 dotnet build -c Release src/components/tbc.core/tbc.core.csproj
 dotnet build -c Release src/components/tbc.target/tbc.target.csproj
+dotnet build -c Release src/components/tbc.host/tbc.host.csproj
+
 dotnet pack -c Release -o output/nuget /p:PackageVersion=0.0.$BUILD_BUILDNUMBER src/components/tbc.core/tbc.core.csproj 
 dotnet pack -c Release -o output/nuget /p:PackageVersion=0.0.$BUILD_BUILDNUMBER src/components/tbc.target/tbc.target.csproj 
+dotnet pack -c Release -o output/nuget /p:PackageVersion=0.0.$BUILD_BUILDNUMBER src/components/tbc.host/tbc.host.csproj 
