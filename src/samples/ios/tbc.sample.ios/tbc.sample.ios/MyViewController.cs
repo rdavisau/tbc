cce9d09075b928005a12bebab01156d0880282b3
Ryan Davis <ryandavis.au@gmail.com>
2022-11-13 22:20:30 +1100

    docs: update ios sample to use dynamic registration

--- src/samples/ios/tbc.sample.ios/tbc.sample.ios/MyViewController.cs
+++ src/samples/ios/tbc.sample.ios/tbc.sample.ios/MyViewController.cs
@@ -1,9 +1,5 @@
 namespace tbc.sample.ios;
 
-// We need this 'register' hack on top of any NSObject-derived type (for now),
-// and it should be different for each type (for now)
-// and it only works on the currently edited file (for now)
-[Register("_MYGUID_MYGUID_MYGUID_MYGUID_MYGUID_")]
 public class MyViewController : UIViewController
 {
     public override void ViewDidLoad()
@@ -12,13 +8,10 @@ public class MyViewController : UIViewController
 
         View!.AddSubview(new UILabel(UIApplication.SharedApplication.Delegate.GetWindow().Frame)
         {
-            Alpha = new nfloat(0),
-            Lines = 0,
-            BackgroundColor = UIColor.Yellow,
+            BackgroundColor = UIColor.Green,
             TextAlignment = UITextAlignment.Center,
             Text = "Hello, iOS",
         });
-
-        UIView.Animate(.25, () => View.Alpha = new nfloat(1.0f));
     }
 }
+
